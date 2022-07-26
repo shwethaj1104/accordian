8594868fea687c7b3d55c77965b5e45fca5b2c11
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-26 20:37:34 +0530

    changes for accordian

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({title,info}) => {
+const [showInfo,setShowInfo] = useState(false)
+
+const changeBtn = ()=>{
+  setShowInfo(!showInfo)
+}
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={changeBtn}>
+          {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {showInfo ? <p>{info}</p> : ''}
+      
+    </article>
+  )
 };
 
 export default Question;
