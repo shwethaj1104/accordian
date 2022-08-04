a512f512a12b42028be5a0db1cb18abc4cdb9fe4
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-04 19:03:47 +0530

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  const [questions,setQuestions] = useState(data)
+  // const [questions,setQuestions] = useState(data)
 
   return (
       <main>
         <div className='container'>
-          <h3>Questions and Answers abour Login</h3>
+          <h3>Questions and Answers about Login</h3>
           <section className='info'>
               {data.map((question) =>{
                 return <SingleQuestion key={question.id} {...question}/>
--- src/Question.js
+++ src/Question.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = ({title,info}) => {
-const [showInfo,setShowInfo] = useState(false)
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false)
 
-const changeBtn = ()=>{
-  setShowInfo(!showInfo)
-}
+  const changeBtn = () => {
+    setShowInfo(!showInfo)
+  }
   return (
     <article className='question'>
       <header>
         <h4>{title}</h4>
         <button className='btn' onClick={changeBtn}>
-          {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
       {showInfo ? <p>{info}</p> : ''}
-      
+
     </article>
   )
 };
